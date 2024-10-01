2e6724ae573210207d2603e660902c05ae6679a5
KovDarlin <ki27.kovalenko.daryna@vtc.vn.ua>
2024-10-01 21:40:53 +0300

    Ex 1 from hm was done

--- main.cpp
+++ main.cpp
@@ -1,6 +1,58 @@
 #include <iostream>
 using namespace std;
 
+bool resArr(int* arr, int size, int num) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == num) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
 int main() {
 
+    int size = 0;
+    int M;
+    cout << "Enter a number of elements for array A: " << endl;
+    cin >> M;
+    int N;
+    cout << "Enter a number of elements for array B: " << endl;
+    cin >> N;
+
+    int* arrA = new int[M];
+    int* arrB = new int[N];
+    int* arrZ = new int[M];
+
+    cout << "Enter elements for array A: " << endl;
+    for (int i = 0; i < M; i++) {
+        cin >> arrA[i];
+    }
+
+    cout << "Enter elements for array B: " << endl;
+    for (int i = 0; i < N; i++) {
+        cin >> arrB[i];
+    }
+
+
+    for (int i = 0; i < M; i++) {
+        bool foundInB = resArr(arrB, N, arrA[i]);
+        if (!foundInB) {
+            arrZ[size] = arrA[i];
+            size++;
+        }
+    }
+
+    cout << "Array Z: " << endl;
+    for (int i = 0; i < size; i++) {
+        cout << arrZ[i] << " ";
+    }
+    cout << endl;
+
+    delete[] arrA;
+    delete[] arrB;
+    delete[] arrZ;
+
+    return 0;
 }
